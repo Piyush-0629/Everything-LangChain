f17d2f2eed36e181757079327569ef94815545ee
Piyush suryawanshi <89908938+Piyush-0629@users.noreply.github.com>
2026-08-09 12:45:07 +0530

    How to load different types of data like csv, pdf.

---
+++ 7.Document Loaders/webbase_loader.py
@@ -0,0 +1,25 @@
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_openai import ChatOpenAI
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = ChatOpenAI(model='gpt-4o-mini')
+
+prompt = PromptTemplate(
+    template='Answer the following question \n {question} from the following text - \n {text}',
+    input_variables=['question','text']
+)
+
+parser = StrOutputParser()
+
+url = 'https://en.wikipedia.org/wiki/LangChain'
+loader = WebBaseLoader(url)
+
+docs = loader.load()
+
+chain = prompt | model | parser
+
+print(chain.invoke({'question':'What is langchian?', 'text':docs[0].page_content}))
